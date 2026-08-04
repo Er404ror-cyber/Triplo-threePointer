5b9e2bfe31612c8847ec8527048e5589a5e1ee03
Badru640 <sbsbadroo@gmail.com>
2026-08-04 20:34:19 +0530

    update:1

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { supabase } from "../lib/supabaseClient";
 import AdminLayout from "../layout/adminLayout";
 
